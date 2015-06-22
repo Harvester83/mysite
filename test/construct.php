1ff205f23f551ceadd8b0b01936745c685240918
Dik83 <rashad.suleymanov83@gmail.com>
2015-06-23 01:41:19 +0500

    Конструкторы и деструкторы

--- test/construct.php
+++ test/construct.php
@@ -1,15 +1,38 @@
 <?php
-class MyDestructableClass {
-
-
-    function __destruct() {
-        print "Уничтожается " . $this->name . "\n";
+class A
+{
+    function __construct()
+    {
+        $a = func_get_args();
+        $i = func_num_args();
+        if (method_exists($this,$f='__construct'.$i)) {
+            call_user_func_array(array($this,$f),$a);
+        }
     }
 
-    function __construct() {
-        print "Конструктор</br>";
-        $this->name = "MyDestructableClass";
+    function __construct1($a1)
+    {
+        echo('__construct with 1 param called: '.$a1.PHP_EOL);
+    }
+
+    function __construct2($a1,$a2)
+    {
+        echo('__construct with 2 params called: '.$a1.','.$a2.PHP_EOL);
+    }
+
+    function __construct3($a1,$a2,$a3)
+    {
+        echo('__construct with 3 params called: '.$a1.','.$a2.','.$a3.PHP_EOL);
     }
 }
 
-$obj = new MyDestructableClass();
+$o = new A('sheep','cat');
+echo '<br>';
+$o = new A('sheep');
+echo '<br>';
+$o = new A('sheep','cat','dog');
+
+// results:
+// __construct with 1 param called: sheep
+// __construct with 2 params called: sheep,cat
+// __construct with 3 params called: sheep,cat,dog

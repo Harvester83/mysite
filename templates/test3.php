a6c030467667dab2ed6c74dd400ac470cc3915f3
Dik83 <rashad.suleymanov83@gmail.com>
2015-10-30 03:08:41 +0400

    Конструкторы и деструкторы

--- templates/test3.php
+++ templates/test3.php
@@ -58,6 +58,82 @@ $cars = array(
         ),
     ),
 );
+$years = array(
+    2015 => 2015,
+    2014 => 2014,
+    2013 => 2013,
+    2012 => 2012,
+    2011 => 2011,
+    2010 => 2010,
+    2009 => 2009,
+    2008 => 2008,
+    2007 => 2007,
+    2006 => 2006,
+    2005 => 2005,
+    2004 => 2004,
+    2003 => 2003,
+    2002 => 2002,
+    2001 => 2001,
+    2000 => 2000,
+    1999 => 1999,
+    1998 => 1998,
+    1997 => 1997,
+    1996 => 1996,
+    1995 => 1995,
+    1994 => 1994,
+    1993 => 1993,
+    1992 => 1992,
+    1991 => 1991,
+    1990 => 1990,
+    1989 => 1989,
+    1988 => 1988,
+    1987 => 1987,
+    1986 => 1986,
+    1985 => 1985,
+    1984 => 1984,
+    1983 => 1983,
+    1982 => 1982,
+    1981 => 1981,
+    1980 => 1980,
+    1979 => 1979,
+    1978 => 1978,
+    1977 => 1977,
+    1976 => 1976,
+    1975 => 1975,
+    1974 => 1974,
+    1973 => 1973,
+    1972 => 1972,
+    1971 => 1971,
+    1970 => 1970,
+    1969 => 1969,
+    1968 => 1968,
+    1967 => 1967,
+    1966 => 1966,
+    1965 => 1965,
+    1964 => 1964,
+    1963 => 1963,
+    1962 => 1962,
+    1961 => 1961,
+    1960 => 1960,
+    1959 => 1959,
+    1958 => 1958,
+    1957 => 1957,
+    1956 => 1956,
+    1955 => 1955,
+);
+
+$years2 = array(
+    1 => 2015,
+    2 => 2014,
+    3 => 2013,
+    4 => 2012,
+    5 => 2011,
+    6 => 2010,
+    7 => 2009,
+    8 => 2008,
+    9 => 2007,
+
+);
 ?>
 <!DOCTYPE html >
 <html>
@@ -84,7 +160,12 @@ $cars = array(
             height: 30px;
             font-size: 16px;
             font-weight: bold;
-            margin-top: 100px;
+            margin-top: 20px;
+        }
+
+        p {
+            padding: 0;
+            margin: 0;
         }
     </style>
 </head>
@@ -92,11 +173,7 @@ $cars = array(
 <div class="container">
 
     <h1><?php echo print_r($cars[1]); ?></h1>
-    <!--<div>
-        <?php /*foreach ($carModels as $carModel => $carModelValue) {*/?>
-            <p><?php /*print_r( $carModel); */?></p>
-        <?php /*}; */?>
-    </div>-->
+
 
     <!-- select models cars drom array-->
     <select id="car-brand">
@@ -113,6 +190,54 @@ $cars = array(
             <option><?php print $carModelValue; ?></option>
         <?php }; ?>
     </select>
+
+    <select id="car-brand">
+        <?php foreach ($years2 as $yearkey => $yearValue) { ?>
+            <option><?php print $yearkey.' ------------ '.$yearValue; ?></option>
+        <?php }; ?>
+    </select>
+
+    <div class="input integer required auto_mileage">
+
+        <input   type="number">
+    </div>
+
+    <p>*********************************************************************************************</p>
+    <p>*********************************************************************************************</p>
+    <p>*********************************************************************************************</p>
+    <p>*********************************************************************************************</p>
+
+
+    <h5>
+        <?php
+            echo "<pre>";
+                print_r(($cars[1]));
+            echo "</pre>";
+        ?>
+    </h5>
+
+    <h5><?php print(($cars[1]['brand'])); ?></h5>
+
+    <div>
+        <?php $carIndificator = 5; ?>
+        <select id="car-brand">
+                <option><?php print(($cars[$carIndificator]['brand'])); ?></option>
+        </select>
+    </div>
+
+    <!-- select models cars drom array-->
+    <div>
+        <select id="car-brand">
+            <?php  foreach ($cars[$carIndificator]['model'] as $models) { ?>
+                    <option><?php print $models; ?></option>
+                <?php }; ?>
+        </select>
+    </div>
+    <script>
+
+
+    </script>
+
 
 </div>
 </body>

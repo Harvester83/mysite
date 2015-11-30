b3faba0d6c5ad98ee80e5ff42003444190b574aa
Rashad <rashad.suleymanov83@gmail.com>
2015-11-30 16:27:41 +0400

    Test JS

--- media/js/main.js
+++ media/js/main.js
@@ -1,3 +1,44 @@
-var longString = 'https://bitbucket.org/Harvester83/oluxana/commits/\b\b6604fd984489c616a6c07f43cb6141447a580a1c';
+var arr = [1, 4, 5];
 
-console.log(longString);
+//console.log(arr[1]);
+
+arr[100] = 25;
+
+delete arr[100];
+
+//console.log(arr[100]);
+
+
+var red = 4/0;
+
+var matrix = [
+  [2, 4, 5],
+  [56, 1, 52],
+  [13, 3, 9]
+];
+
+//console.log(matrix[1][1]);
+
+var myFish1 = ['angel', 'clown', 'mandarin', 'surgeon'];
+var myFish2 = ['angel', 'clown', 'mandarin', 'surgeon'];
+
+var removed1 = myFish1.splice(2, 0, 'drum');
+var removed2 = myFish2.splice(2, 3, 'drum');
+
+
+//console.log(myFish1);
+//console.log(myFish2);
+
+
+var obj = {
+    propOne: 'One',
+    propTwo: 'two'
+},
+
+
+prop;
+
+for(prop in obj) {
+    if(obj.hasOwnProperty(prop))
+        console.log(prop + ': ' + obj[prop]);
+}

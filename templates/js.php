cc075dfabc330d9a00bb5ee14edb050fa31b4745
Rashad <rashad.suleymanov83@gmail.com>
2015-12-10 12:46:15 +0400

    TEST JS delegation

--- templates/js.php
+++ templates/js.php
@@ -1,21 +1,63 @@
-<p>Привет камартики</p>
+<!--<div>
+    <div class="box">
+        <h2>BOX BLOCK</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+            a
+            type specimen book. It has survived not only five centuries, but also the leap</p>
+    </div>
 
-<p>Привет камартики</p>
+    <div id="demo">
+        <h2>DEMO BLOCK</h2>
 
-<p>Привет камартики</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+            a
+            type specimen book. It has survived not only five centuries, but also the leap</p>
+    </div>
+    <div class="clearer"></div>
+</div>-->
 
-<div class="box">
-    <h2>BOX BLOCK</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-        type specimen book. It has survived not only five centuries, but also the leap</p>
-</div>
+<button id="add">Add string</button>
+<table class="zebra">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Product name</th>
+            <th>Price</th>
+            <th>Description</th>
+        </tr>
+    </thead>
 
-<div id="demo">
-    <h2>DEMO BLOCK</h2>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-        type specimen book. It has survived not only five centuries, but also the leap</p>
-</div>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Product name 1</td>
+            <td>Price 1</td>
+            <td>Description 2</td>
+        </tr>
 
-<script src="media/js/test.js"></script>
+        <tr>
+            <td>2</td>
+            <td>Product name 2</td>
+            <td>Price 2</td>
+            <td>Description 2</td>
+        </tr>
+
+        <tr>
+            <td>3</td>
+            <td>Product name 3</td>
+            <td>Price 3</td>
+            <td>Description 3</td>
+        </tr>
+
+       <tr>
+            <td>4</td>
+            <td>Product name 4</td>
+            <td>Price 4</td>
+            <td>Description 4</td>
+        </tr>
+    </tbody>
+</table>
+
+<script src="media/js/test-delegation.js"></script>
